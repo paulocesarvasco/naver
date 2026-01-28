1fb9c3931ca4a8fca783da5d5fc1ffac804b0d48
Vice <paulo.vasco@st-one.io>
2026-01-28 13:49:46 -0300

    feat: improve composite API scanning to exhaust all result pages

--- src/service.ts
+++ src/service.ts
@@ -1,16 +1,25 @@
 import { firefox, Browser, BrowserContext, Page } from 'playwright';
 import { createOEPConfigCookie, createSessionCookie } from './helpers/cookies';
 import { env } from './config/env';
+import { extractScanParameters, replaceScanParameters } from './helpers/url';
+import EventEmitter from 'events';
 
 const referer =
   'https://search.shopping.naver.com/ns/search?query=iphone&includedDeliveryFee=true&score=4.8%7C5';
-const url =
-  'https://search.shopping.naver.com/ns/v1/search/paged-composite-cards?cursor=50&pageSize=50&query=iphone&searchMethod=all.basic&isFreshCategory=false&isOriginalQuerySearch=false&isCatalogDiversifyOff=false&listPage=1&categoryIdsForPromotions=50000204&categoryIdsForPromotions=50000205&hiddenNonProductCard=true&hasMoreAd=true&hasMore=true&score=4.8|';
 
-export class BrowserSession {
+export function delay(ms: number, jitterMs: number = 0): Promise<void> {
+  const jitter = jitterMs > 0 ? Math.floor(Math.random() * jitterMs) : 0;
+
+  return new Promise((resolve) => setTimeout(resolve, ms + jitter));
+}
+
+export class BrowserSession extends EventEmitter {
   private browser!: Browser;
   private context!: BrowserContext;
-  private page!: Page;
+  private cursor!: number;
+  private listPage!: number;
+  private pageSize!: number;
+  private targetUrl!: string;
 
   constructor(
     private readonly options: {
@@ -23,7 +32,9 @@ export class BrowserSession {
       userAgent?: string;
       headless?: boolean;
     },
-  ) {}
+  ) {
+    super();
+  }
 
   async init() {
     this.browser = await firefox.launch({
@@ -35,30 +46,40 @@ export class BrowserSession {
       locale: this.options.locale ?? 'ko-KR',
       userAgent: this.options.userAgent,
     });
-
-    this.page = await this.context.newPage();
-    this.attachListeners();
   }
 
-  private attachListeners() {
-    this.page.on('request', (req) => {
+  private attachListeners(page: Page) {
+    page.on('request', (req) => {
       if (req.resourceType() === 'document') {
-        console.log('➡️ DOCUMENT REQUEST', req.url());
-        console.log('Headers:', req.headers());
+        console.log('DOCUMENT REQUEST: ', req.url());
+        // console.log('Headers:', req.headers());
       }
     });
 
-    this.page.on('response', async (res) => {
+    page.on('response', async (res) => {
       if (res.request().resourceType() !== 'document') return;
 
-      console.log('⬅️ DOCUMENT RESPONSE', res.status());
+      console.log('DOCUMENT RESPONSE: ', res.status());
+
+      if (res.status() != 200 && res.status() != 407) {
+        process.exit(1);
+      }
 
       const headers = res.headers();
 
       if (!headers['content-type']?.includes('application/json')) return;
 
       const prod = await res.json();
-      console.log(prod.data.data[0].card.product);
+      if (prod.data.hasMore) {
+        this.cursor += this.pageSize;
+        if (this.cursor >= this.pageSize * this.listPage * 2) {
+          this.listPage++;
+        }
+        await res.finished();
+        this.emit('next_request');
+      } else {
+        await this.close();
+      }
     });
   }
 
@@ -66,15 +87,45 @@ export class BrowserSession {
     await this.context.addCookies([createSessionCookie(), createOEPConfigCookie()]);
   }
 
-  async goto(url: string, referer?: string) {
-    await this.page.goto(url, {
-      referer,
-      waitUntil: 'domcontentloaded',
-    });
+  async clearCookies() {
+    await this.context.clearCookies();
+  }
+
+  setScanParameters(url: string) {
+    const scanParameters = extractScanParameters(url);
+    this.targetUrl = url;
+    this.cursor = scanParameters.cursor;
+    this.listPage = scanParameters.listPage;
+    this.pageSize = scanParameters.pageSize;
+  }
+
+  updateScanParameters() {
+    this.targetUrl = replaceScanParameters(
+      this.targetUrl,
+      this.cursor,
+      this.listPage,
+      this.pageSize,
+    );
+  }
+
+  async goto(referer?: string) {
+    try {
+      const page = await this.context.newPage();
+      this.attachListeners(page);
+      await page.goto(this.targetUrl, {
+        referer,
+        waitUntil: 'domcontentloaded',
+      });
+      await page.close();
+    } catch (err) {
+      console.log(err);
+      this.emit('reset_proxy');
+    }
   }
 
   async close() {
-    await this.browser?.close();
+    await this.context.close();
+    await this.browser.close();
   }
 }
 
@@ -87,7 +138,22 @@ const session = new BrowserSession({
   },
 });
 
+session.on('next_request', async () => {
+  // await delay(100, 2000);
+  await session.clearCookies();
+  await session.setCookies();
+  session.updateScanParameters();
+  await session.goto(referer);
+});
+
+session.on('reset_proxy', () => {
+  session.init();
+  session.emit('next_request');
+});
+
 await session.init();
 await session.setCookies();
-await session.goto(url, referer);
-await session.close();
+session.setScanParameters(
+  'https://search.shopping.naver.com/ns/v1/search/paged-composite-cards?cursor=1&pageSize=50&query=iphone&listPage=1',
+);
+await session.goto(referer);
